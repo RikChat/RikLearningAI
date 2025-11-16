e4ce079761352c48d09f586f67fd1df19ad35edf
Thoriq Ichsani <108530206+RikChat@users.noreply.github.com>
2025-11-16 10:05:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+
+  // Hapus experimental.appDir karena Next 14 sudah otomatis memakai app router
+  experimental: {},
+
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-    ],
-  },
-}
-module.exports = nextConfig
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig;
